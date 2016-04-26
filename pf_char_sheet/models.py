b1be2ea7c8677ff8d74a449277ba89b6d93d0f17
V Morishima <waver.morishima@booking.com>
2016-04-26 12:26:24 -0700

    'fixing'

--- pf_char_sheet/models.py
+++ pf_char_sheet/models.py
@@ -39,6 +39,10 @@ class Character(models.Model):
     }
     size = models.CharField(max_length=1, choices=size_choices, default='m')
     #char_img = models.ImageField()
+    inventory = models.TextField(default="")
+    GP = models.IntegerField(default=0)
+    SP = models.IntegerField(default=0)
+    CP = models.IntegerField(default=0)
 
     # Stats
     def calculate_ability_modifier(self, ability):
@@ -76,23 +80,28 @@ class Character(models.Model):
     wis_modifier = property(_get_wis_modifier)
 
     HP = models.IntegerField(default=10)
-    FORT = models.IntegerField(default=10)
-    WILL = models.IntegerField(default=10)
-    REFLEX = models.IntegerField(default=10)
+    FORT = models.IntegerField(default=0, verbose_name="Base Fortitude")
+    WILL = models.IntegerField(default=0, verbose_name="Base Will")
+    REFLEX = models.IntegerField(default=0, verbose_name="Base Reflex")
 
     def _get_fort_save(self):
-        return self.FORT + self.calculate_ability_modifier(self.CON)
+        return self.FORT + self.con_modifier
     fort_save = property(_get_fort_save)
 
     def _get_will_save(self):
-        return self.WILL + self.calculate_ability_modifier(self.WIS)
+        return self.WILL + self.wis_modifier
     will_save = property(_get_will_save)
 
     def _get_reflex_save(self):
-        return self.REFLEX + self.calculate_ability_modifier(self.DEX)
+        return self.REFLEX + self.dex_modifier
     reflex_save = property(_get_reflex_save)
 
-    BAB = models.IntegerField(default=10)
+    BAB = models.IntegerField(default=0, verbose_name="Base Attack Bonus")
+    base_AC = models.IntegerField(default=0, verbose_name="Armor Bonuses")
+
+    def _get_ac(self):
+        return 10 + self.base_AC + self.size_modifier + self.dex_modifier
+    AC = property(_get_ac)
 
     def get_absolute_url(self):
         return '/%i/' % self.id
@@ -122,13 +131,43 @@ class Character(models.Model):
     size_modifier = property(_get_size_modifier)
 
     def _get_cmb(self):
-        return self.BAB + self.calculate_ability_modifier(self.STR) + self.size_modifier
+        return self.BAB + self.str_modifier + self.size_modifier
     CMB = property(_get_cmb)
 
     def _get_cmd(self):
-        return self.BAB + self.calculate_ability_modifier(self.STR) + self.calculate_ability_modifier(self.DEX) + self.size_modifier + 10
+        return self.BAB + self.str_modifier + self.dex_modifier + self.size_modifier + 10
     CMD = property(_get_cmd)
 
+    # SKILLS
 
-    def roll_d20(self):
-        return randint(1,20)
+    acrobatics = models.IntegerField(default=0)
+    appraise = models.IntegerField(default=0)
+    bluff = models.IntegerField(default=0)
+    climb = models.IntegerField(default=0)
+    diplomacy = models.IntegerField(default=0)
+    disable_device = models.IntegerField(default=0)
+    disguise = models.IntegerField(default=0)
+    escape_artist = models.IntegerField(default=0)
+    fly = models.IntegerField(default=0)
+    handle_animal = models.IntegerField(default=0)
+    heal = models.IntegerField(default=0)
+    intimidate = models.IntegerField(default=0)
+    knowledge_arcana = models.IntegerField(default=0)
+    knowledge_dungeoneering = models.IntegerField(default=0)
+    knowledge_engineering = models.IntegerField(default=0)
+    knowledge_geography = models.IntegerField(default=0)
+    knowledge_history = models.IntegerField(default=0)
+    knowledge_nature = models.IntegerField(default=0)
+    knowledge_nobility = models.IntegerField(default=0)
+    knowledge_planes = models.IntegerField(default=0)
+    knowledge_religion = models.IntegerField(default=0)
+    linguistics = models.IntegerField(default=0)
+    perception = models.IntegerField(default=0)
+    ride = models.IntegerField(default=0)
+    sense_motive = models.IntegerField(default=0)
+    sleight_of_hand = models.IntegerField(default=0)
+    spellcraft = models.IntegerField(default=0)
+    stealth = models.IntegerField(default=0)
+    survival = models.IntegerField(default=0)
+    swim = models.IntegerField(default=0)
+    use_magic_device = models.IntegerField(default=0)
